611eb255d0ebd6f3a518a016102d82ea197c7733
Andrew Ryu <andrewryu@outlook.com>
2022-04-24 15:11:22 -0700

    added prompts

--- index.js
+++ index.js
@@ -9,9 +9,115 @@
 // THEN that GitHub profile opens in a new tab
 // WHEN I start the application
 // THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+  
+// {
+//   "dependencies": {
+//     "inquirer": "^8.2.2",
+//     "jest": "^27.5.1"
+//   }
+// }
 
 const inquirer = require('inquirer')
 const Manager = require('./lib/Manager')
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
-const fs = require('fs')
+const fs = require('fs')  //enable interaction with the file system
+const path = require('./src/generateHtml')  //link to create page
+// const path = require('path')
+
+// start the app
+// create a team
+    // enter manager's name, id, email, officeNumber
+    // add ee
+        // option: engineer
+            // enter name, id, email, github username
+        // option: intern
+            // enter name, id, email, school
+// HTML file is generated
+    // click email address <a href>
+    // click github username <a href>
+
+const managerInfo = ()=>{
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "What is the manager's name?",
+            name: "managerName"
+        },
+        {
+            type: 'input',
+            message: "What is the manager's id?",
+            name: "managerId"
+        },
+        {
+            type: 'input',
+            message: "What is the manager's email address?",
+            name: "managerEmail"
+        },
+        {
+            type: 'input',
+            message: "What is the manager's office number?",
+            name: "managerOfficeNumber"
+        },
+    ]).then(ans => {
+        console.log(ans)
+
+    })
+}
+ 
+
+const engineerInfo = ()=>{
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "What is the engineer's name?",
+            name: "engineerName"
+        },
+        {
+            type: 'input',
+            message: "What is the engineer's id?",
+            name: "engineerName"
+        },
+        {
+            type: 'input',
+            message: "What is the engineer's email?",
+            name: "engineerEmail"
+        },
+        {
+            type: 'input',
+            message: "What is the engineer's GitHub username?",
+            name: "engineerGithub"
+        },
+    ]).then(ans => {
+        console.log(ans)
+    })
+}
+
+
+const internInfo = ()=>{
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "What is the intern's name?",
+            name: "internName"
+        },
+        {
+            type: 'input',
+            message: "What is the intern's id?",
+            name: "internName"
+        },
+        {
+            type: 'input',
+            message: "What is the intern's email?",
+            name: "internEmail"
+        },
+        {
+            type: 'input',
+            message: "Which school is the intern attening or graduate from?",
+            name: "internSchool"
+        },
+    ]).then(ans => {
+        console.log(ans)
+    })
+}
+
