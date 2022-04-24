43646ff0a0cd56122736c87e84143d5289cd0e94
Andrew Ryu <andrewryu@outlook.com>
2022-04-23 22:39:35 -0700

    added classes

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 // takes in info about employees on a SW engineering team, then generates an HTML webpage that displays summaries for each person. 
 // write a unit test for every part of the code and ensure it passes
-// 
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for my team members and their information
+// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
+// WHEN I click on an email address in the HTML
+// THEN my default email program opens and populates the TO field of the email with the address
+// WHEN I click on the GitHub username
+// THEN that GitHub profile opens in a new tab
+// WHEN I start the application
+// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+
+const inquirer = require('inquirer')
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const fs = require('fs')
