c9c0c669b381491fc9f24c8b98e08d778f8f6bb3
Andrew Ryu <andrewryu@outlook.com>
2022-04-23 21:38:01 -0700

    set up files

---
+++ index.js
@@ -0,0 +1,3 @@
+// takes in info about employees on a SW engineering team, then generates an HTML webpage that displays summaries for each person. 
+// write a unit test for every part of the code and ensure it passes
+// 
